73b5e0ba24d0d7b8f9461bea718900a16c67f89d
mrpinkcat <gatien.anizan@gmail.com>
2023-02-13 18:53:32 +0100

    fix: change tickrate

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ enum ServerStatus {
   RUNNING = 'running',
 };
 
-const tick = 20 * 1000;
+const tick = 60 * 1000;
 let minecraftStack: Stack | null = null;
 let token: string | null = null;
 let stopTimeout: number | null = null;
